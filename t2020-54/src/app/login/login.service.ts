548338ae639b295397a4ca3bfcf4f21d98a433cc
kwanlamyu <kwanlamyujoey@gmail.com>
2020-01-07 13:28:48 +0800

    login services update

--- t2020-54/src/app/login/login.service.ts
+++ t2020-54/src/app/login/login.service.ts
@@ -10,15 +10,16 @@ export class LoginService {
 
   login (userId: string) {
     console.log('@login Getting user id');
-    let url = "http://192.168.50.103:3000/customerId";
+    let url = "http://192.168.50.103:3000/login";
     let request = {
       userId: userId
     }
     return new Promise(
       (resolve, reject) => {
         this.http.post(url, request).subscribe(
-          (response : any) => {
-            console.log("customer Id " + response);
+          response  => {
+            console.log("customer Id received");
+            console.log(response);
             resolve(response);
           },
           error=> {
