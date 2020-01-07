4b850cb8b28b3663c72882e478417c1716b244c6
Chin Wei Boon <chinweiboon@hotmail.com>
2020-01-07 12:18:03 +0800

    Initial commit for API calls

---
+++ server.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//template obj to send a request
+var options = {
+  //placeholder
+  //url: "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/" + req.body.userName,
+  //
+  url: "",
+  method: "GET",
+  headers: {
+    identity: "T77",
+    token: "d74f68a7-ade9-4996-b74e-985353d31892"
+  }
+};
+//req.body.<name of form>
+//homepage/login page
+
+app.get("/", function(req, res) {
+  //send home screen html
+  //res.sendFile(__dirname + "");
+  res.send("<h1>Hello!</h1>");
+});
+
+// Customer ID
+app.get("/customerId", function(req, res) {
+  //http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/:userName
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+//customer details
+app.get("/customerDetails", function(req, res) {
+  //http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/:customerId/details
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/2/details";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+
+//TRANSACTIONS
+//Transaction details
+app.get("/transactionDetails", function(req, res) {
+  //http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/:accountId?from=01-01-2018&to=01-30-2020
+  //needs accountID and date to check
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/79?from=01-01-2018&to=01-30-2020";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+
+//List of Deposit Accounts
+app.get("/depositAccounts", function(req, res) {
+  //"http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/:customerId"
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/2";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+//Get balance of deposit accounts
+app.get("/depositAccountsBalance", function(req, res) {
+  //"http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/:accountId/balance?month=1&year=2018"
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/79/balance?month=0&year=2018";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+//List of Marketing Messages
+app.get("/marketingMessages", function(req, res) {
+  //"http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing"
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+//Details of Marketing Messages
+app.get("/marketingMessageDetails", function(req, res) {
+  //"http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing/:id"
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing/1";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+//Personal Messages
+app.get("/personalMessage", function(req, res) {
+  //"http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/message/:customerId"
+  options.url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/message/2";
+  request(options, function(error, response, body){
+    //convert JSON to javascript obj
+    //var data = JSON.parse(body);
+    //console.log(data);
+    res.write(body);
+    res.send();
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
